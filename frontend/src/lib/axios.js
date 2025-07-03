ea8a3c338bc6e4c043c43f11c48cf74244688c48
Jana1902 <jana7cr@gmail.com>
2025-07-04 01:16:52 +0530

    Optimized with updated links2

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chat-application-m53e.onrender.com",
+  baseURL: "https://chat-application-m53e.onrender.com/api",
   withCredentials: true,
 });
